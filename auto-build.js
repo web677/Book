46daa1517f49edf04fda46914fb0196a130507e4
laoli <407907175@qq.com>
2017-12-27 21:39:51 +0800

    添加book自动更新构建

---
+++ auto-build.js
@@ -0,0 +1,50 @@
+const { spawn } = require('child_process')
+const nodemailer = require('nodemailer')
+
+setInterval(() => {
+    let gitPull = spawn("git", ["pull"])
+
+    gitPull.stdout.on("data", data => {
+        let gitbookBuild = spawn("gitbook", ["build"])
+        let transporter = nodemailer.createTransport({
+            service: "qq",
+            auth: {
+                user: "407907175@qq.com",
+                pass: "yqxhzqgnpmmzbghj"
+            }
+        })
+
+        let mailOptions = {
+            from: '"来自老李的邮件👻" <407907175@qq.com>',
+            to: 'ly@fanli.com',
+            subject: 'Hello 老李',
+            text: '你的book更新成功并自动构建成功啦',
+            html: '<b>哈哈哈</b>'
+        }
+
+        transporter.sendMail(mailOptions)
+    })
+
+    gitPull.stderr.on("data", data => {
+        let transporter = nodemailer.createTransport({
+            service: "qq",
+            auth: {
+                user: "407907175@qq.com",
+                pass: "yqxhzqgnpmmzbghj"
+            }
+        })
+
+        let mailOptions = {
+            from: '"来自老李的邮件👻" <407907175@qq.com>',
+            to: 'ly@fanli.com',
+            subject: 'Hello 老李',
+            text: '你的book更新失败啦，无法自动构建了，快去看看',
+            html: '<b>别墨迹</b>'
+        }
+
+        transporter.sendMail(mailOptions)
+    })
+
+},1000)
+
+
