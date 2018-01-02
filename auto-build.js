b40a7b90be234a486e0d983b5d772691647efae3
laoli <407907175@qq.com>
2018-01-02 16:42:02 +0800

    test

--- auto-build.js
+++ auto-build.js
@@ -4,7 +4,7 @@ var handler = createHandler({ path: '', secret: '123qwe,,,'})
 
 http.createServer(function (req, res) {
     handler(req, res, function (err) {
-        console.log(JSON.stringify(req))
+        console.log(req)
         res.statusCode = 200
         res.end('yeah got it')
     })
