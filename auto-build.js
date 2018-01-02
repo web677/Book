60fb2feb4365118b3d4408f432663ed41652aebb
laoli <407907175@qq.com>
2018-01-02 16:54:47 +0800

    test

--- auto-build.js
+++ auto-build.js
@@ -29,6 +29,7 @@ handler.on('issues', function (event) {
 })
 
 // test
+// test
 
 // const http = require('http')
 // const githubWebhook = require('github-webhook-handler')({path: '', secret: '123qwe,,,'})
